e8db483b3ebcf14da75276a8bf0955cb152e1018
mrbrunelli <matheus.brunelli@gmail.com>
2022-07-19 16:10:59 -0300

    centraliza a fonte no rodapé do sticker

--- src/main/java/com/example/GenerateSticker.java
+++ src/main/java/com/example/GenerateSticker.java
@@ -2,6 +2,7 @@ package com.example;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -16,14 +17,22 @@ public class GenerateSticker {
 
         int width = original.getWidth();
         int height = original.getHeight();
+        int heightFactor = height / 100 * 20;
 
-        BufferedImage image = new BufferedImage(width, height + 200, BufferedImage.TRANSLUCENT);
+        BufferedImage image = new BufferedImage(width, height + heightFactor, BufferedImage.TRANSLUCENT);
 
         Graphics2D drawer = (Graphics2D) image.getGraphics();
         drawer.drawImage(original, 0, 0, null);
+
+        Font font = new Font(Font.SERIF, Font.BOLD, width / 10);
+
+        FontMetrics metrics = drawer.getFontMetrics(font);
+        int fontPositionX = (width - metrics.stringWidth(text)) / 2;
+        int fontPositionY = height + (heightFactor / 2);
+
+        drawer.setFont(font);
         drawer.setColor(Color.YELLOW);
-        drawer.setFont(new Font(Font.SERIF, Font.BOLD, 40));
-        drawer.drawString(text, 50, height + 100);
+        drawer.drawString(text, fontPositionX, fontPositionY);
 
         ImageIO.write(image, "png", new File(path + ".png"));
     }
