b67c8a4d8656ff746bd7901a37d178442c04c34a
mrbrunelli <matheus.brunelli@gmail.com>
2022-07-20 08:10:02 -0300

    refatora e melhora legibilidade

--- src/main/java/com/example/GenerateSticker.java
+++ src/main/java/com/example/GenerateSticker.java
@@ -12,28 +12,39 @@ import java.io.InputStream;
 import javax.imageio.ImageIO;
 
 public class GenerateSticker {
+    private int width;
+    private int height;
+
     public void generate(InputStream input, String text, String path) throws IOException {
         BufferedImage original = ImageIO.read(input);
 
-        int width = original.getWidth();
-        int height = original.getHeight();
-        int heightFactor = height / 100 * 20;
+        width = original.getWidth();
+        height = original.getHeight();
+        int extraHeight = getExtraHeight();
 
-        BufferedImage image = new BufferedImage(width, height + heightFactor, BufferedImage.TRANSLUCENT);
+        BufferedImage image = new BufferedImage(width, height + extraHeight, BufferedImage.TRANSLUCENT);
 
         Graphics2D drawer = (Graphics2D) image.getGraphics();
         drawer.drawImage(original, 0, 0, null);
 
-        Font font = new Font(Font.SERIF, Font.BOLD, width / 10);
+        Font font = new Font(Font.SERIF, Font.BOLD, getFontSize());
 
         FontMetrics metrics = drawer.getFontMetrics(font);
         int fontPositionX = (width - metrics.stringWidth(text)) / 2;
-        int fontPositionY = height + (heightFactor / 2);
+        int fontPositionY = height + (extraHeight / 2);
 
         drawer.setFont(font);
         drawer.setColor(Color.YELLOW);
         drawer.drawString(text, fontPositionX, fontPositionY);
 
         ImageIO.write(image, "png", new File(path + ".png"));
+    }
+
+    private int getExtraHeight() {
+        return (int) (height * 0.20);
+    }
+
+    private int getFontSize() {
+        return width / 10;
     }
 }
